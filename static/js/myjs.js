28a70a2453768beacb0ec76e3045c1f64d576ec8
dtkienn <dtkien12300@gmail.com>
2020-12-23 04:41:44 +0700

    added a search bar

---
+++ static/js/myjs.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".submit").click(function(){
+        //document.getElementsByClassName("search-dropdown").toggle("show");
+        document.getElementsByTagName()
+    })
+})
