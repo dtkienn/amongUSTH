cb51d921bd0455891eec8c92363598210df40c89
JeffKi11er <45094606+JeffKi11er@users.noreply.github.com>
2021-01-14 21:18:24 +0700

    generate password and create Login_info

---
+++ javadoc/src/com/company/archive/DataAccess.java
@@ -0,0 +1,4 @@
+package com.company.archive;
+@Tag("div")
+public class DataAccess {
+}
--- login/mongo.py
+++ login/mongo.py
@@ -1,5 +1,6 @@
 import pymongo
 from login.User import user as usr
+from flask_login import UserMixin
 
 client = pymongo.MongoClient("mongodb+srv://Sm00thiee:123@cluster0.3ihx5.mongodb.net/?retryWrites=true&w=majority")
 
@@ -11,7 +12,11 @@ u_stu = user['Student']
 u_lec = user['Lecturer']
 
 
-class User:
+class User(UserMixin):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
     def register(id_, name, email, profile_pic):
         if u_info.find_one({'Email' : email}):
             print('Existed!')
@@ -32,9 +37,34 @@ class User:
         mdict = {'UID' : id_, 'USTH_ID' : usth_id, 'Major': major, 'SchoolYear' : schoolYear}
         u_stu.insert_one(mdict)
 
+    @staticmethod
+    def get(user_id):
+        db = get_db()
+        usr = db.execute(
+            "SELECT * FROM user WHERE id = ?", (user_id,)
+        ).fetchone()
+        if not usr:
+            return None
+
+        usr = user(
+            id_=usr[0], name=usr[1], email=usr[2]#, profile_pic=user[3]
+        )
+        return usr
+
+    def login(username,password):
+        mdict = u_login.find_one({'UserName' : username}, {'UserName' :1,'Password' :1})
+        print("///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////"+str(mdict)+"/////////////////////////////////////////////////////////////////////////////////")
+        if password == mdict["Password"]:
+            return User(username, password)
+        return None
+
     def add_info_lec(id_, department):
         mdict = {'UID' : id_, 'Department' : department}
         u_lec.insert_one(mdict)
+
+    def add_login_info(id_, username, password):
+        mdict = {'UID' : id_, 'UserName' : username,'Password' : password}  
+        u_login.insert_one(mdict)
 
     def get_profile_pic(id_):
         mdict = u_info.find_one({'UID' : id_}, {'Profile_pic' : 1, '_id' : 0})
@@ -51,3 +81,30 @@ class User:
     def get_id(username):
         mdict = u_login.find_one({'Username' : username})
         return mdict['UID']
+
+# class Login_info:
+#     def login_info_register(id_, username, email, password, profile_pic):
+#         if u_login.find_one({'Email' : email}):
+#             print('Existed')
+#             pass
+#         else:
+#             mdict = {'UID' : id_, 'UserName' : name, 'Email' : email, 'Password' : password, 'Profile_pic' : profile_pic}
+#             u_login.insert_one(mdict)
+
+#     def get_profile_pic(id_):
+#         mdict = u_login.find_one({'UID' : id_}, {'Profile_pic' : 1, '_id' : 0})
+#         return mdict['Profile_pic']
+
+#     def get_name(id_):
+#         mdict = u_login.find_one({'UID' : id_}, {'Fullname' : 1, '_id' : 0})
+#         return mdict['Fullname']
+
+#     def get_email(id_):
+#         mdict = u_login.find_one({'UID' : id_}, {'Email' : 1, '_id' : 0})
+#         return mdict['Email']
+
+#     def get_id(username):
+#         mdict = u_login.find_one({'Username' : username})
+#         return mdict['UID']        
+                
+#         
