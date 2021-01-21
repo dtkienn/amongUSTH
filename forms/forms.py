f80331f2cd9870bba9c64d9d4b2106f8bcee4309
JeffKi11er <45094606+JeffKi11er@users.noreply.github.com>
2021-01-21 08:48:35 +0700

    Create forms.py

---
+++ forms/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class Password(FlaskForm):
+	current_password = PasswordField('Current Password',validators=[DataRequired()])
+	new_password = PasswordField('New Password',validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('new_password')])
+	submit = SubmitField('Change!')
+
+class BookPost(FlaskForm):
+	file_name = StringField('File Name',validators=[DataRequired()])
+	description = StringField('Description',validators=[DataRequired()])
+	file = StringField('File',validators=[DataRequired()])
+	submit = SubmitField('Post !')
