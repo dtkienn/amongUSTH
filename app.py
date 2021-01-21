24864749c1e84ea4cc5a2693a8976dad7ec20191
Sm00thiee <nhanlq2013@gmaill.com>
2021-01-21 16:38:14 +0700

    Minor update in login security ;)

--- app.py
+++ app.py
@@ -23,11 +23,14 @@ from login.mongo import User as mongoUsr
 from flask_bcrypt import Bcrypt
 from forms.forms import Password
 # Configuration
-GOOGLE_CLIENT_ID = '754525070220-c2lfse3erd1rk52lvas6orr9im9ojkp3.apps.googleusercontent.com'
-GOOGLE_CLIENT_SECRET = '7TMNNst1I5ueVjacoQDa1sJg'
-GOOGLE_DISCOVERY_URL = (
-    "https://accounts.google.com/.well-known/openid-configuration"
-)
+import json
+data = json.load(open('app_key.json'))
+client_key = data['google_login'][0]['client_key']
+client_secret = data['google_login'][0]['client_secret']
+discovery_url = data['google_login'][0]['discovery_url']
+GOOGLE_CLIENT_ID = client_key
+GOOGLE_CLIENT_SECRET = client_secret
+GOOGLE_DISCOVERY_URL = discovery_url
 
 # Flask app setup
 app = Flask(__name__)
@@ -116,6 +119,7 @@ def login():
             redirect_uri=request.base_url + "/callback",
             scope=["openid", "email", "profile"],
         )
+        print (request_uri)
         return redirect(request_uri)
 
 
@@ -155,44 +159,36 @@ def callback():
         users_email = userinfo_response.json()["email"]
         picture = userinfo_response.json()["picture"]
         users_name = userinfo_response.json()["name"]
-    else:
-        return "User email not available or not verified by Google.", 400
+        if "@st.usth.edu.vn" in users_email or '@usth.edu.vn' in users_email:
+            # Add user information to Online database
+            global user            
+            user = logUsr.user_info(
+                id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+            )
+            id_ = user.get_id()
+            name = user.getName()
+            # temp = name.split()
+            # name = temp[1] + ' ' + temp[2] + ' ' + temp[0]
+            email = user.getEmail()
+            profile_pic = user.getprofile_pic()
+            mongoUsr.register(id_, name, email, profile_pic)
+            generate_password()
+            # Doesn't exist? Add to database
+            if not user.get(unique_id):
+                user.create(unique_id, users_name, users_email, picture)
+                # Begin user session by logging the user in
+            login_user(user)
 
-    # Create a user in our db with the information provided
-    # by Google
+            # Create session timeout
+            time = timedelta(minutes=60)
+            # User will automagically kicked from session after 'time'
+            app.permanent_session_lifetime = time
+            
+            return redirect(url_for('index'))     
+        else:
+            return redirect(url_for('loginfail'))
 
-    global user
-    user = logUsr.user_info(
-        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
-    )
-    # Doesn't exist? Add to database
-    if not user.get(unique_id):
-        user.create(unique_id, users_name, users_email, picture)
-
-    if "@st.usth.edu.vn" in users_email:
-        # Begin user session by logging the user in
-        login_user(user)
-
-        # Create session timeout
-        time = timedelta(minutes=60)
-        # User will automagically kicked from session after 'time'
-        app.permanent_session_lifetime = time
-
-        # Add user information to Online database
-        id_ = user.get_id()
-        name = user.getName()
-        # temp = name.split()
-        # name = temp[1] + ' ' + temp[2] + ' ' + temp[0]
-        email = user.getEmail()
-        profile_pic = user.getprofile_pic()
-        mongoUsr.register(id_, name, email, profile_pic)
-
-        generate_password()
-        return redirect(url_for('index'))       
-
-    else:
-        logout_user()
-        return redirect(url_for('loginfail'))
+      
 
 @app.route('/loginfail')
 def loginfail():
@@ -203,8 +199,8 @@ def loginfail():
 @login_required
 def logout():
     logout_user()
+    print('Logged out')
     return redirect(url_for("homepage"))
-
 
 def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
